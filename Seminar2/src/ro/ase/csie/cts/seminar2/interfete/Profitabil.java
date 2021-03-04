dd629b4a01c49d15e758f9d904064a6ba2d514c9
codru <stefancodruta18@gmail.com>
2021-03-04 12:28:20 +0200

    first comm

--- Seminar2/src/ro/ase/csie/cts/seminar2/interfete/Profitabil.java
+++ Seminar2/src/ro/ase/csie/cts/seminar2/interfete/Profitabil.java
@@ -1,5 +1,14 @@
 package ro.ase.csie.cts.seminar2.interfete;
 
+//interfata contine metode abstracte 
+//poate contine atribute statice
+//NU POATE CONTINTE NIMIC ALTCEVA
 public interface Profitabil {
+	//final pt metode -- nu poate fi suprascrisa
+	//final pt o clasa -- nu paote fi derivata
+	
+	public static final int MAX_PROCENT_DOBANDA = 5;
+	
+		public abstract void adaugaDobanda(double RataDobanziiProcente);
+	}
 
-}
