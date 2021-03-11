cc4f010caec976b1e5b4076ef5ec8a255fb1bb48
codru <stefancodruta18@gmail.com>
2021-03-11 12:35:38 +0200

    commitpauza

--- Seminar3_/src/CodFaza1/Produs.java
+++ Seminar3_/src/CodFaza1/Produs.java
@@ -1,26 +1,30 @@
 package CodFaza1;
 
 public class Produs{
+	//statice pt a nu fi schimbate la runtime 
+	public static final int VECHIME_CLIENT_MAXIMA = 10;
+	public static final float DISCOUNT_CLIENT_MAXIM = 0.15f;
 	
-	public float CalculeazaPretFinal(int tipprodus, float pretInitial, int vechimeClientiInAni)
+	
+	public float CalculeazaPretFinal(TipProdus tipprodus, float pretInitial, int vechimeClientiInAni)
 	{
 	    float pretFinal = 0;
 	    
-	    float Discount_fidelitate = (vechimeClientiInAni > 10) ? (float)15/100 : (float)vechimeClientiInAni/100; 
+	    float Discount_fidelitate = (vechimeClientiInAni > VECHIME_CLIENT_MAXIMA) ? DISCOUNT_CLIENT_MAXIM : (float)vechimeClientiInAni/100; 
 	    
-	    if (tipprodus == 1)
+	    if (tipprodus == TipProdus.NOU)
 	    {
 	      pretFinal = pretInitial;
 	    }
-	    else if (tipprodus == 2)
+	    else if (tipprodus == TipProdus.CU_DISCOUNT)
 	    {
 	      pretFinal = (pretInitial - (0.1f * pretInitial)) - Discount_fidelitate * (pretInitial - (0.1f * pretInitial));
 	    }
-	    else if (tipprodus == 3)
+	    else if (tipprodus == TipProdus.STOC_LIMITAT)
 	    {
 	      pretFinal = (pretInitial - (0.25f * pretInitial)) - Discount_fidelitate * (pretInitial - (0.25f * pretInitial));
 	    }
-	    else if (tipprodus == 4)
+	    else if (tipprodus == TipProdus.VECHI )
 	    {
 	      pretFinal = (pretInitial - (0.35f * pretInitial)) - Discount_fidelitate * (pretInitial - (0.35f * pretInitial));
 	    }
---
+++ Seminar3_/src/CodFaza1/TipProdus.java
@@ -0,0 +1,20 @@
+package CodFaza1;
+
+//enumerarile sunt clase 
+//referinte catre obiecte
+public enum TipProdus {
+
+	NOU(0),CU_DISCOUNT(0.1f),STOC_LIMITAT(0.25f),VECHI(0.35f);
+	
+	private final  float discount;
+	
+	//nu putem public -> pt a nu adauga simblouri 
+	private TipProdus(float discount) {
+		this.discount=discount;
+	}
+	
+	public float getDiscount() {
+		return this.discount;
+	}
+	
+}
